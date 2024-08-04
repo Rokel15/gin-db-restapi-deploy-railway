d22b95173c3efef8da4edd65e6927698896a0d87
Rokel15 <dab.loam15@gmail.com>
2024-08-04 10:31:27 +0700

    update

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 	"golang-gin-db-restapi-lokal/database"
 	"golang-gin-db-restapi-lokal/routers"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "liatdibawahlaptop"
-	dbName   = "for_testing1"
-)
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "postgres"
+// 	password = "liatdibawahlaptop"
+// 	dbName   = "for_testing1"
+// )
 
 var (
 	DB  *sql.DB
@@ -26,10 +27,23 @@ var (
 func main() {
 	var PORT = ":8080"
 
-	psqlInfo := fmt.Sprintf(`host=%s port=%d user=%s password=%s dbname=%s sslmode=disable`, host, port, user, password, dbName)
+	// psqlInfo := fmt.Sprintf(`host=%s port=%d user=%s password=%s dbname=%s sslmode=disable`,
+	// 	host,
+	// 	port,
+	// 	user,
+	// 	password,
+	// 	dbName,
+	// )
+
+	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 
 	DB, err = sql.Open("postgres", psqlInfo)
-
 	if err != nil {
 		log.Fatalf("Error Opening Database: %v\n", err)
 	}
